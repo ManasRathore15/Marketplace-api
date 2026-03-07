e3ede93f5f8a539c7bd5765410fed61702f27c29
Manas Rathore <manasrathore15@gmail.com>
2026-03-07 12:50:16 +0530

    Add Ad model and register it in admin

--- ads/models.py
+++ ads/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Ad(models.Model):
+    title = models.CharField(max_length=255)
+    discription = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    location = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User,on_delete=models.CASCADE, related_name= "ads")
+
+    def __str__(self):
+        return self.title
+    
