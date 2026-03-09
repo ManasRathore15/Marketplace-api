576bb4d7071105cd625ac02481c20a43ec259d80
Manas Rathore <manasrathore15@gmail.com>
2026-03-10 01:06:30 +0530

    Added Image upload feature for ads

--- ads/models.py
+++ ads/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+
 # Create your models here.
 
 class Ad(models.Model):
@@ -10,6 +12,8 @@ class Ad(models.Model):
     location = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     owner = models.ForeignKey(User,on_delete=models.CASCADE, related_name= "ads")
+
+    image = models.ImageField(upload_to='ads/', null=True, blank=True)
 
     def __str__(self):
         return self.title
