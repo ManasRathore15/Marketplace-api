627c1d56a3256fb7e4750d22237ec0414b431a0d
Manas Rathore <manasrathore15@gmail.com>
2026-03-10 19:13:24 +0530

    Prepair Project for Deployment

--- config/settings.py
+++ config/settings.py
@@ -21,12 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&xz9*v0)tgtx783z7c&+=t-q_00(a0_e2ndlw(m9+^nu-2l)g&'
-
+SECRET_KEY = os.environ.get("SECRET_KEY","dev-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -119,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES":(
